c85fed0cccbb8abd5a6add3971ee3560d77dcd59
Nasir <nasir.rubel99@gmail.com>
2022-10-07 23:40:26 +0600

    apps list update

--- src/utils/apps.ts
+++ src/utils/apps.ts
@@ -4,9 +4,9 @@ export interface AppListItem {
 }
 
 const apps: { [key: string]: AppListItem } = {
-  b2b_uae: {
-    andorid: `https://play.google.com/store/apps/`,
-    ios: `https://www.apple.com/app-store/`,
+  "b2b-uae": {
+    andorid: `https://play.google.com/store/apps/details?id=com.flyhub.b2b.uae`,
+    ios: `https://apps.apple.com/app/flyhub-agent-uae/id6443584285`,
   },
 };
 
