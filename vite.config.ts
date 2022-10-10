4fdbbf1fc53706a138d63ee0641dc5a8c768ff41
Nasir <nasir.rubel99@gmail.com>
2022-10-10 14:10:25 +0600

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from "vite";
+/** @format */
+
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
+import { DateTime } from "luxon";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint({ cache: false }), splitVendorChunkPlugin()],
+  build: {
+    outDir: "./dist",
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].` + DateTime.now().toUnixInteger() + `.js`,
+        chunkFileNames: `[name].` + DateTime.now().toUnixInteger() + `.js`,
+        assetFileNames: `[name].` + DateTime.now().toUnixInteger() + `.[ext]`,
+      },
+    },
+  },
 });
