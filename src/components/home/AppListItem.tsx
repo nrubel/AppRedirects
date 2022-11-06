104ed331c476431e98288700e7b69a437eb14248
Nasir <nasir.rubel99@gmail.com>
2022-11-06 13:26:59 +0600

    app item responsive

--- src/components/home/AppListItem.tsx
+++ src/components/home/AppListItem.tsx
@@ -1,34 +1,53 @@
 /** @format */
 
-import { Box, CircularProgress, Grid, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  CircularProgress,
+  Grid,
+  Stack,
+  Theme,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
 import { IFRedirect } from "hrms_types";
 import { FC, Suspense } from "react";
-import Endpoints from "../../utils/Endpoints";
 import appStore from "../../../public/appStore.svg";
 import playStore from "../../../public/playStore.svg";
+import Endpoints from "../../utils/Endpoints";
 
 const AppListItem: FC<{ item: IFRedirect; isOdd: boolean }> = ({
   item,
   isOdd,
 }) => {
   const { path, android, cover, ios, name, coverMeta, country } = item;
+  const mdMatch = useMediaQuery(useTheme().breakpoints.up("md"));
 
   return (
     <Box
-      sx={{
+      sx={(theme: Theme) => ({
         backgroundColor: "#FFFFFF",
         borderRadius: 4,
         mb: 3.75,
-        p: 10,
-      }}
+        p: mdMatch ? 10 : 5,
+      })}
     >
       <Grid
-        columnSpacing={10}
+        columnSpacing={mdMatch ? 10 : 2}
         container
         alignItems={"center"}
         flexDirection={isOdd ? "row" : "row-reverse"}
       >
-        <Grid item xs={12} md={5}>
+        <Grid
+          item
+          xs={12}
+          md={5}
+          sx={(theme: Theme) => ({
+            [theme.breakpoints.down("md")]: {
+              textAlign: "center",
+            },
+          })}
+        >
           <Suspense
             fallback={
               <Box sx={coverMeta}>
@@ -49,17 +68,30 @@ const AppListItem: FC<{ item: IFRedirect; isOdd: boolean }> = ({
         <Grid item xs={12} md={7}>
           <Typography
             component={"h2"}
-            sx={{
+            sx={(theme: Theme) => ({
               mb: 2,
-              fontSize: 44,
+              fontSize: mdMatch ? 44 : 32,
               fontWeight: 700,
               lineHeight: 1.23,
-            }}
+              [theme.breakpoints.down("md")]: {
+                mt: 2,
+                textAlign: "center",
+              },
+            })}
           >
             {name}
           </Typography>
           {!!country?.code && (
-            <Stack direction={"row"} alignItems={"center"} sx={{ mb: 5 }}>
+            <Stack
+              direction={"row"}
+              alignItems={"center"}
+              sx={(theme: Theme) => ({
+                mb: mdMatch ? 5 : 3,
+                [theme.breakpoints.down("md")]: {
+                  justifyContent: "center",
+                },
+              })}
+            >
               <Suspense
                 fallback={
                   <Box sx={{ width: 36, height: 26 }}>
@@ -91,29 +123,35 @@ const AppListItem: FC<{ item: IFRedirect; isOdd: boolean }> = ({
               </Typography>
             </Stack>
           )}
-          <Stack direction={"row"} spacing={2}>
+          <Stack
+            direction={"row"}
+            spacing={2}
+            sx={(theme: Theme) => ({
+              img: {
+                maxWidth: "47%",
+              },
+              [theme.breakpoints.down("md")]: {
+                justifyContent: "center",
+              },
+            })}
+          >
             {!!android && (
               <Box
                 component={"img"}
                 src={playStore}
                 alt={"install app"}
                 loading={"lazy"}
-                width={189}
-                height={56}
-                style={{ cursor: "pointer" }}
+                sx={{ cursor: "pointer" }}
                 onClick={() => window.open(android, "_self")}
               />
             )}
-            <Box sx={{ ml: 2 }} />
             {!!ios && (
               <Box
                 component={"img"}
                 src={appStore}
                 alt={"get app"}
                 loading={"lazy"}
-                width={189}
-                height={56}
-                style={{ cursor: "pointer" }}
+                sx={{ cursor: "pointer", ml: 2 }}
                 onClick={() => window.open(ios, "_self")}
               />
             )}
